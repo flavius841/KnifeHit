4250fd68ca2c0d2d809fdf17bb638354163ad1b2
flavius841 <flaviusepintilie@gmail.com>
2026-02-14 12:02:06 +0200

    Home Page Knives pt2

--- Assets/Resources/Script/HomePageKnifes.cs
+++ Assets/Resources/Script/HomePageKnifes.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+
 public class HomePageKnifes : MonoBehaviour
 {
     [SerializeField] List<GameObject> knives;
@@ -8,7 +9,7 @@ public class HomePageKnifes : MonoBehaviour
     [SerializeField] float spawnTime;
     [SerializeField] float Timer;
     [SerializeField] float speed;
-    [SerializeField] int LeftToRight;
+    [SerializeField] int LeftOrRight;
     [SerializeField] int Right = 1;
     [SerializeField] int Left = 0;
     [SerializeField] float Rotation;
@@ -16,39 +17,46 @@ public class HomePageKnifes : MonoBehaviour
 
     void Start()
     {
-
+        spawnTime = Random.Range(5f, 10f);
     }
 
     void Update()
     {
         foreach (var knife in knives)
         {
-            spawnTime = Random.Range(0.5f, 2f);
+
             Timer += Time.deltaTime;
 
             if (Timer >= spawnTime)
             {
+                spawnTime = Random.Range(15f, 20f);
                 Timer = 0;
-                LeftToRight = random.randint(0, 1);
+                LeftOrRight = Random.Range(0, 2);
+                //LeftOrRight = 0;
 
-                if (LeftToRight == Right)
+                if (LeftOrRight == Right)
                 {
-
+                    Rotation = Random.Range(-210f, -120f);
                 }
 
                 else
                 {
                     Rotation = Random.Range(-40f, 50f);
-                    knife.transform.rotation = Quaternion.Euler(0, 0, Rotation);
-                    knife.transform.position = new Vector3(0, 0, 0);
-
-
                 }
 
-
+                knife.transform.rotation = Quaternion.Euler(0, 0, Rotation);
+                knife.transform.position = new Vector3(0, 0, 0);
             }
+
+            else
+            {
+                knife.transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+            }
+
+
 
         }
 
     }
+
 }
