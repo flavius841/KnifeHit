115fbc838761b1a87832dc3200addf3a56b30b26
flavius841 <flaviusepintilie@gmail.com>
2026-02-14 11:34:52 +0200

    Home Page Knives

--- Assets/Resources/Script/HomePageKnifes.cs
+++ Assets/Resources/Script/HomePageKnifes.cs
@@ -5,18 +5,50 @@ public class HomePageKnifes : MonoBehaviour
 {
     [SerializeField] List<GameObject> knives;
     [SerializeField] GameObject knifePrefab;
+    [SerializeField] float spawnTime;
+    [SerializeField] float Timer;
+    [SerializeField] float speed;
+    [SerializeField] int LeftToRight;
+    [SerializeField] int Right = 1;
+    [SerializeField] int Left = 0;
+    [SerializeField] float Rotation;
+    [SerializeField] float Limit;
 
     void Start()
     {
-        foreach (var knife in knives)
-        {
-            knives.Add(knifePrefab);
-            // knife.SetActive(true);
-        }
+
     }
 
     void Update()
     {
+        foreach (var knife in knives)
+        {
+            spawnTime = Random.Range(0.5f, 2f);
+            Timer += Time.deltaTime;
+
+            if (Timer >= spawnTime)
+            {
+                Timer = 0;
+                LeftToRight = random.randint(0, 1);
+
+                if (LeftToRight == Right)
+                {
+
+                }
+
+                else
+                {
+                    Rotation = Random.Range(-40f, 50f);
+                    knife.transform.rotation = Quaternion.Euler(0, 0, Rotation);
+                    knife.transform.position = new Vector3(0, 0, 0);
+
+
+                }
+
+
+            }
+
+        }
 
     }
 }
